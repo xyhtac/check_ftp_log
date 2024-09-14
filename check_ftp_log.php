32c472ca806203c366ee908e197940767aa40a17
xyhtac <73641145+xyhtac@users.noreply.github.com>
2024-09-14 12:38:24 +0200

    Update check_ftp_log.php

--- check_ftp_log.php
+++ check_ftp_log.php
@@ -277,11 +277,15 @@ if ( $cfg['data-source'] == 'log'  ) {
 		if ( isset( $res[6][0] ) ) {
 			$datetime = $res[3][0]."-". $res[2][0]."-". $res[1][0]." ". $res[4][0].":". $res[5][0].":". $res[6][0];
 			$file_time = strtotime($datetime);
-			echo $file_time;
 			$backup_age = $now_time - $file_time;
 		}
 				
 		if ( preg_match("/".$cfg['bak-file-pattern']."/",$fname) && $backup_age < $newest && isset( $res[7][0] ) ) { 
+			$newest = $backup_age;
+			$filename = $fname;
+			$newesttime = $datetime;
+			$flag = $res[7][0];
+		} elseif ( preg_match($cfg['bak-file-pattern'].$date_pattern,$fname) && $backup_age < $newest && isset( $res[7][0] ) ) {
 			$newest = $backup_age;
 			$filename = $fname;
 			$newesttime = $datetime;
